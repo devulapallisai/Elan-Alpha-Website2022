712ec916505a56577523264899b79c4542a73b78
devulapallisai <ee20btech11013@iith.ac.in>
2022-01-31 09:34:43 +0530

    fixed hamburger issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../images/elannvision.png";
 import styles from "../css/Header.module.css";
@@ -48,7 +48,7 @@ function Header() {
           </div>
         </div>
         {/* Mobile part begins now */}
-        <div className="w-full">
+        <div className="w-full lg:hidden">
           <div className="bg-gradient-to-b from-blue-100 to-transparent h-auto">
             <div className="imgsrc pl-4 inline-block py-4">
               <Link to="/">
@@ -57,33 +57,35 @@ function Header() {
             </div>
           </div>
         </div>
-        <div
-          className={`fixed right-0 top-0 flex flex-col top-0 text-white right-0 lg:hidden z-2 content-end m-2  ${style.animateit}`}
-          style={{
-            borderBottomLeftRadius: 10,
-            borderTopLeftRadius: 10,
-            borderTopRightRadius: 10,
-            borderBottomRightRadius: 10,
-            border: hamburger ? "2px solid yellow" : "none",
-            background: hamburger ? "yellow" : "none",
-          }} 
-        >
-          <div className="flex flex-row-reverse">
-            <div
-              className={
-                hamburger
-                  ? `${style.btn1} ${style.active} z-50`
-                  : `${style.btn1} ${style.notActive} z-50`
-              }
-              onClick={() => sethamburger((prev) => !prev)}
-            >
-              <span className={style.span1}></span>
-              <span className={style.span1}></span>
-              <span className={style.span1}></span>
-            </div>
+        <div className="flex flex-row-reverse lg:hidden">
+          <div
+            className={
+              hamburger
+                ? `${style.btn1} ${style.active} z-50 fixed top-0 right-0`
+                : `${style.btn1} ${style.notActive} z-50 fixed top-0 right-0`
+            }
+            onClick={() => sethamburger((prev) => !prev)}
+          >
+            <span className={style.span1}></span>
+            <span className={style.span1}></span>
+            <span className={style.span1}></span>
           </div>
-          {hamburger ? (
-            <div className="flex flex-col list-none mr-5 ml-2 w-200 mb-2" data-aos="fade-left" data-aos-duration="400">
+        </div>
+        {hamburger ? (
+          <div
+            className={`fixed right-0 top-0 flex flex-col top-0 text-white right-0 lg:hidden z-2 content-end m-2 pt-7  ${style.animateit}`}
+            style={{
+              borderBottomLeftRadius: 10,
+              borderTopLeftRadius: 10,
+              borderTopRightRadius: 10,
+              borderBottomRightRadius: 10,
+              border: "2px solid yellow",
+              background: "yellow",
+            }}
+            data-aos="fade-left"
+            data-aos-duration="400"
+          >
+            <div className="flex flex-col list-none mr-5 ml-2 w-200 mb-2">
               {routes.reverse().map((route) => (
                 <Link
                   to={route[1]}
@@ -93,10 +95,10 @@ function Header() {
                 </Link>
               ))}
             </div>
-          ) : (
-            ""
-          )}
-        </div>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React from "react";
 
 function Home() {
-  return (<div className='text-heading'>Home</div>);
+  return (
+    <div className="text-heading">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores dolore
+      at qui numquam, quaerat, quisquam atque nulla doloribus sapiente nesciunt
+      et dicta nobis voluptatibus, quibusdam veritatis harum ducimus similique
+      ipsam aut optio. Aperiam, possimus. Asperiores quasi voluptatum sint,
+      quidem suscipit laudantium eos delectus nobis sequi corporis tempore
+      aliquid eveniet molestiae autem at eum reprehenderit, beatae inventore
+      quas. Voluptatibus sit odio eaque enim a maxime vero delectus fugit minus
+      harum, dolores dolorem. Doloremque, ex rerum sequi eligendi officiis porro
+      optio quae placeat dolor. Quae itaque dolorum doloremque, voluptatibus
+      aliquam id maxime officia! Aperiam beatae voluptate harum, dolorum in
+      officiis. Magni, quaerat! Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Doloribus ipsam doloremque deserunt debitis provident,
+      quis perspiciatis ullam voluptatum perferendis voluptate similique amet
+      est aliquam unde nulla. Itaque, reprehenderit. Reiciendis vel dolorum non
+      minima qui doloremque eos cumque ullam impedit laborum animi alias
+      voluptatem tenetur dicta perspiciatis velit, tempore quae nemo! Aut quidem
+      aliquid, corrupti fugiat voluptates itaque repudiandae dignissimos nam
+      labore distinctio quae est laboriosam repellat odio neque rem dolores
+      aliquam autem nostrum asperiores? Quam, repellendus sequi dicta, ad nulla
+      exercitationem possimus maiores ex excepturi nobis facere. Quo et ab esse
+      veritatis harum vel hic? Tempora libero eligendi doloribus animi! Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Nemo officiis iure,
+      officia expedita similique voluptatum reiciendis sunt ipsa. Aspernatur
+      accusantium, ab animi perferendis quos provident esse praesentium
+      exercitationem libero aliquam, magnam rem, vero beatae omnis a quibusdam
+      numquam dolor fugit nulla voluptatibus facilis repellendus enim! Delectus
+      officiis placeat ex mollitia, esse architecto eum ut nobis accusamus est
+      ipsum labore perspiciatis commodi maiores porro ipsa corrupti quo
+      recusandae ad dolore fugiat natus eligendi repellendus. Magnam saepe
+      quibusdam ipsum repellat dignissimos quae repellendus sunt, consequuntur
+      vero, nobis alias exercitationem quidem doloremque, asperiores accusamus
+      quaerat? Saepe atque mollitia vitae quidem autem, quasi iste! Esse
+      repudiandae, suscipit reiciendis pariatur quam voluptatum exercitationem
+      beatae molestiae ab assumenda mollitia rerum ex praesentium molestias
+      perspiciatis placeat in magni commodi est sapiente at sed. Quis, aliquid
+      aliquam rem modi dolorem corporis in dolores sit quae odit natus
+      repudiandae aperiam, maiores expedita illo officia impedit assumenda
+      voluptas sunt repellat, quibusdam porro? Impedit ipsam atque alias
+      consequuntur laboriosam! Ipsum atque sint impedit fuga id nostrum harum
+      pariatur sunt, accusantium sed, voluptas ipsam, aspernatur repellat
+      incidunt sapiente. Temporibus, omnis est reiciendis et accusantium quis
+      nulla porro soluta ex quam consequatur enim esse inventore exercitationem
+      vel amet eius itaque quia fugit dicta?
+    </div>
+  );
 }
 
 export default Home;
