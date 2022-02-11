9b724618f8a37240f4e4b5c3ac22ee68246c8088
ArunSiddardha <80515168+ArunSiddardha@users.noreply.github.com>
2022-02-12 02:52:45 +0530

    social cause

--- src/pages/ProshowList.js
+++ src/pages/ProshowList.js
@@ -4,8 +4,8 @@ import Zakir from '../images/events/zakir.jpeg'
 import styles from '../css/proshow.module.css'
 function ProshowList() {
   return (
-    <div className="Proshow h-screen flex justify-center items-center w-screen bg-events">
-        <div className="relative container mx-auto flex justify-center items-center ">
+    <div className="relative Proshow flex justify-center items-center h-screen bg-events">
+        <div className=" relative container mx-auto flex justify-center items-center ">
             <Card image= {Zakir} description="dsndkjvh jfknefvk erenkvsdflnfkajnfjsdnvkdjsnvkjdvnkjdfvnjkdfsvkdfnvkjdfnvjkdfnvkjdfbnvjkdfj djfkvdkfvhdfkvhdfkvhdkvhdjv dkfhv dkjfv hdkjfhvkdfhv jkdf hvdkfh vdf vdf hvjkdfh vdf v hdfv df vdfv khdfvh dfkvhdkfvh dfkv hdkf vhdkf vhkdfhvkdfhvdkfhvkdfh vdkf vkdf hvkdf vhkdfhvkjdhvdjkvhs "  />
         </div>
         
--- src/pages/SocialCause.js
+++ src/pages/SocialCause.js
@@ -1,7 +1,48 @@
 import React from 'react';
-
+import styles from '../css/socialcause.module.css'
+import Halfchakra from '../images/SocialCause/Halfchakra.png'
+import Hausla from '../images/SocialCause/hausla.png'
+import Gradient from '../images/SocialCause/Gradient.png'
+import Rectangle from '../images/SocialCause/Rectangle.png'
 function SocialCause() {
-  return <div>Helo</div>;
+  return ( 
+  <div className={`${styles.colorit}`}>
+  <div className={`relative ${styles.colorit}`}>
+    <div className={`absolute mx-auto lg:mx-40 container z-1 w-5/6 lg:w-3/4  ${styles.resizeit}`}>
+      <img src={Halfchakra} alt="" className={styles.flipimageit} />
+    </div>
+    <div className='relative w-1/2 h-1/2 z-3 mx-auto '>
+      <img src = {Hausla} alt=""/>
+    </div>
+    <p className='mx-auto text-sm w-2/3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+  <br />
+  <div className='h-screen'>
+  </div>
+  <div className='flex flex-wrap gap-20 justify-center items-center h-screen'>
+    <div className='w-1/4 '>
+      <img src={Gradient} alt="" className={styles.flipimageit}/>
+    </div>
+    <p className='w-1/2 text-sm'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    </p>
+  </div>
+  <br />
+  <div className='flex flex-wrap justify-center items-center h-screen '>
+    <div className='w-1/4 '>
+      <img src={Rectangle} alt="" className={styles.flipimageit}/>
+    </div>
+    <div className='w-1/3 text-xs border-[#FFD700] p-5 border-y-2 border-r-2'>
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sdfchabsdhmnbcsmhnbcsm
+      "Lorem ipsum dolor sit 
+    </div>
+  </div>
+  <div className="contactsectioninpage">
+        <div className="text-white font-text font-extrabold text-xl md:text-4xl text-center">Contact Us</div>
+  </div>
+</div>
+);
 }
 
 export default SocialCause;
