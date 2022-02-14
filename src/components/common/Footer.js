3a7408c9a8304e2325c19e31ac4bbbad59f7b33f
devulapallisai <ee20btech11013@iith.ac.in>
2022-02-14 23:24:33 +0530

    addedd

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -14,17 +14,25 @@ function Footer() {
   return (
     <div className="hidden md:grid grid-cols-2 p-8">
       <div className="flex flex-row justify-between" style={{ maxWidth: 200 }}>
-        <FontAwesomeIcon icon={faFacebookF} className="text-white text-3xl" />
-        <FontAwesomeIcon icon={faInstagram} className="text-white text-3xl" />
-        <FontAwesomeIcon icon={faYoutube} className="text-white text-3xl" />
-        <FontAwesomeIcon icon={faTwitter} className="text-white text-3xl" />
+        <a href="/contact" target="_blank">
+          <FontAwesomeIcon icon={faFacebookF} className="text-white text-3xl" />
+        </a>
+        <a href="/contact" target="_blank">
+          <FontAwesomeIcon icon={faInstagram} className="text-white text-3xl" />
+        </a>
+        <a href="/contact" target="_blank">
+          <FontAwesomeIcon icon={faYoutube} className="text-white text-3xl" />
+        </a>
+        <a href="/contact" target="_blank">
+          <FontAwesomeIcon icon={faTwitter} className="text-white text-3xl" />
+        </a>
       </div>
       <div className="flex justify-end">
         <div className="flex justify-between">
           {routes.reverse().map((route) => (
             <Link
               to={route[1]}
-              className={`cursor-pointer font-bold text-2xl font-text text-headerwhite my-auto mx-2`}
+              className={`cursor-pointer font-bold text-2xl font-text text-headerwhite my-auto mx-4`}
             >
               {route[0]}
             </Link>
