7cd66b3c4dc229bf66f91f938a21334d9b2c211e
devulapallisai <ee20btech11013@iith.ac.in>
2022-02-18 17:01:22 +0530

    added

--- src/components/Homepage/Secondslide.js
+++ src/components/Homepage/Secondslide.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
 import { gsap } from "gsap";
+import Chakra from "../../images/Homepage/chakra.png";
 import styles from "../../css/Homepage2.module.css";
 import Lanterntop from "../../images/Lantern1.png";
 import Lant from "../../images/Homepage/Lanterntop.png";
 import Skyline from "../../images/Homepage/Skyline3copy.png";
 import BackgroundI from "../../images/Homepage/window_webcopy.png";
+import style from '../../css/Homepage1.module.css';
 function Secondslide() {
+  const textref = useRef();
   return (
     <>
       <div
@@ -36,14 +39,30 @@ function Secondslide() {
         <div className={`${styles.zInde} w-screen absolute bottom-0`}>
           <img src={Skyline} alt="Imag" className={styles.zoomimg} />
         </div>
+        <div className="flex justify-center" style={{height:"auto",zIndex:-100}}>
+          <div className="absolute bottom-4" data-aos="fade-down">
+            <img src={Chakra} alt="chakra" style={{height:"auto",zIndex:-100}} className={`z-100 ${style.chakra}`} />
+          </div>
+        </div>
         <div className="flex justify-center">
           <div
             className={`absolute top-40 xl:top-48 text-sm flex justify-center ${styles.texthere}`}
+            ref={textref}
+            data-aos="fade-down"
+            data-aos-duration="1000"
           >
             <div className="flex flex-col">
-              <div className="font-heading text-3xl 2xl:text-5xl text-center pb-2">Heading</div>
-              <div className="text-center">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo libero commodi voluptatem ad tenetur aspernatur, molestiae veritatis maxime, iure earum est. Eligendi explicabo beatae, doloribus tenetur ea velit rem magnam laborum laboriosam sit sapiente alias nihil aliquid eveniet, tempora a officiis dolore ullam rerum quam commodi labore vero! Magnam modi non eos, ipsa sed omnis laboriosam numquam iure architecto aperiam voluptatum laborum atque dolor aliquid ad quae quaerat. Consequuntur molestiae deserunt ipsam porro quas laboriosam sed unde placeat. Odit earum accusamus, recusandae exercitationem, dolorem aliquam, dolor similique harum dolores dignissimos aspernatur mollitia ab sint. Dignissimos, facilis nemo. Magnam, quam ut!
+              <div className="font-heading text-3xl 2xl:text-5xl text-center pb-2">
+                OUR THEME
+              </div>
+              <div className="text-center font-text">
+                This year Elan and ηvision celebrates childhood - the most
+                important phase, capable of shaping and moulding lives. While
+                most of us are in our late teens or early twenties, we help you
+                to relax a bit to take a jog down your memory lane. Fables of a
+                Moppet, our theme will help you to rejoice those times when we
+                hid Roald Dahl's books in textbooks, when a popsicle could make
+                us happy and when stress was just a physics term.
               </div>
             </div>
           </div>
