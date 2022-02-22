1b3225a14788102e6602b3139bfab0d4c7e09906
ArunSiddardha <80515168+ArunSiddardha@users.noreply.github.com>
2022-02-23 01:12:45 +0530

    Done

--- src/components/Homepage/ThirdSlide.js
+++ src/components/Homepage/ThirdSlide.js
@@ -42,13 +42,16 @@ function ThirdSlide() {
         duration:3,
 
     })
-
+    gsap.to('Guha',{
+        opacity:0,
+        duration :3,
+    })
   },[])
   return (
     <div className={`Back relative flex justify-center h-screen w-screen ${styles.bgit}`}>
-        <div className='Guha absolute z-[1000] h-screen'>
-        <img src={BackgroundI} style={{width:'100vw',height:'100vh'}}></img>
-        </div>
+        {/* <div className='Guha absolute z-[1000] h-screen'>
+        <img className='Guha2' src={BackgroundI} style={{width:'100vw',height:'100vh'}}></img>
+        </div> */}
         <div className="Para absolute w-2/3 mx-auto top-[15vh]">
             <h1 className='font-heading text-white text-2xl'>Heading</h1>
             <p className='text-white text-sm'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
