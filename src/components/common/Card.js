899f0cec78272a0d0a6c989b969b3520aeea7061
devulapallisai <ee20btech11013@iith.ac.in>
2022-02-26 15:28:55 +0530

    added

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -2,14 +2,16 @@ import React, { useState, useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import styles from "../../css/team.module.css";
-import Goldenbox from '../../images/Golden Box.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {  faPhoneSquare } from '@fortawesome/free-solid-svg-icons'
-function Card({name,position,image,type,phone}) {
+import Goldenbox from "../../images/Golden Box.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhoneSquare } from "@fortawesome/free-solid-svg-icons";
+function Card({ name, position, image, type, phone }) {
   return (
     <div className={`m-4`}>
-      <div className={`${styles.card}` } data-aos="flip-up">
-        <div className={`${styles.box} d-flex justify-center mx-auto relative ${type}`}>
+      <div className={`${styles.card}`} data-aos="flip-up">
+        <div
+          className={`${styles.box} d-flex justify-center mx-auto relative ${type}`}
+        >
           <img src={image} className={`${styles.box}`} alt="ImageHere" />
           <div className={`absolute top-2 left-2 ${styles.box}`}>
             <img src={Goldenbox} alt="ImageHere" />
@@ -21,8 +23,8 @@ function Card({name,position,image,type,phone}) {
         <div className="text-center font-text text-2xl  text-white">
           {position}
         </div>
-        <div className="text-center font-text text-2xl  text-white">
-        <FontAwesomeIcon icon={faPhoneSquare} className="px-2 fa-rotate-90"/>
+        <div className="text-center font-text text-xl  text-white">
+          <FontAwesomeIcon icon={faPhoneSquare} className="mr-1" />
           <a href={`tel:${phone}`}>{phone}</a>
         </div>
       </div>
