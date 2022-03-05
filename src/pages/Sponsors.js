15523b327a977614428a8e6ea7d366bb46743875
devulapallisai <ee20btech11013@iith.ac.in>
2022-03-05 16:01:31 +0530

    added

--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -3,6 +3,7 @@ import styles from "../css/sponsors.module.css";
 import Halfchakra from "../images/SocialCause/Halfchakra.png";
 import useWindowDimensions from "../hooks/useWindowDimensions";
 import Contactsection from "../components/sponsors/contactsection";
+import Sponsorcard from "../components/sponsors/Sponsorcard";
 function Sponsors() {
   let { width, height } = useWindowDimensions();
   return (
@@ -29,73 +30,34 @@ function Sponsors() {
         </div>
         <br />
         {/* Here comes the sponsors part */}
-        <div className="sponsors w-screen">
-          <div class={`${styles.container1}`}>
-            <div class={`${styles.content}`}>
-              <div class={`${styles.contentOverlay}`}></div>
-              <img
-                alt="img"
-                class={`${styles.contentImage}`}
-                src="https://images.unsplash.com/photo-1433360405326-e50f909805b3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=359e8e12304ffa04a38627a157fc3362"
-              />
-              <div class={`${styles.fadeInTop} ${styles.contentDetails}`}>
-                <h3>This is a title</h3>
-                <p>This is a short description</p>
-              </div>
-            </div>
-          </div>
-          <div class={`${styles.container1}`}>
-            <div class={`${styles.content}`}>
-              <div class={`${styles.contentOverlay}`}></div>
-              <img
-                alt="img"
-                class={`${styles.contentImage}`}
-                src="https://images.unsplash.com/photo-1433360405326-e50f909805b3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=359e8e12304ffa04a38627a157fc3362"
-              />
-              <div class={`${styles.fadeInTop} ${styles.contentDetails}`}>
-                <h3>This is a title</h3>
-                <p>This is a short description</p>
-              </div>
-            </div>
-          </div>
-          <div class={`${styles.container1}`}>
-            <div class={`${styles.content}`}>
-              <div class={`${styles.contentOverlay}`}></div>
-              <img
-                alt="img"
-                class={`${styles.contentImage}`}
-                src="https://images.unsplash.com/photo-1433360405326-e50f909805b3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=359e8e12304ffa04a38627a157fc3362"
-              />
-              <div class={`${styles.fadeInTop} ${styles.contentDetails}`}>
-                <h3>This is a title</h3>
-                <p>This is a short description</p>
-              </div>
-            </div>
-          </div>
-          <div class={`${styles.container1}`}>
-            <div class={`${styles.content}`}>
-              <div class={`${styles.contentOverlay}`}></div>
-              <img
-                alt="img"
-                class={`${styles.contentImage}`}
-                src="https://images.unsplash.com/photo-1433360405326-e50f909805b3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=359e8e12304ffa04a38627a157fc3362"
-              />
-              <div class={`${styles.fadeInTop} ${styles.contentDetails}`}>
-                <h3 className="font-heading text-lg">This is a title</h3>
-                <p className="font-text text-sm overflow-y-scroll">
-                  This is a short description Lorem ipsum dolor sit amet
-                  consectetur adipisicing elit. Voluptatem quisquam ad alias
-                  distinctio accusamus qui at adipisci illum illo expedita
-                  aspernatur, culpa
-                </p>
-              </div>
-            </div>
-          </div>
+        <div className="sponsors w-screen justify-center items-center">
+          <Sponsorcard
+            Title="Sponsor"
+            Description="Sample Descritption"
+            Image="https://images.unsplash.com/photo-1433360405326-e50f909805b3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=359e8e12304ffa04a38627a157fc3362"
+          />
+          <Sponsorcard
+            Title="Sponsor"
+            Description="Sample Descritption"
+            Image="https://images.unsplash.com/photo-1433360405326-e50f909805b3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=359e8e12304ffa04a38627a157fc3362"
+          />
+          <Sponsorcard
+            Title="Sponsor"
+            Description="Sample Descritption"
+            Image="https://images.unsplash.com/photo-1433360405326-e50f909805b3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=359e8e12304ffa04a38627a157fc3362"
+          />
+          <Sponsorcard
+            Title="Sponsor"
+            Description="Sample Descritption"
+            Image="https://images.unsplash.com/photo-1433360405326-e50f909805b3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=359e8e12304ffa04a38627a157fc3362"
+          />
         </div>
       </div>
       <div
         className={`${styles.colorit} contactsectioninpage relative bottom-0 w-full`}
       >
+        <br />
+        <br />
         <div className="text-white font-text font-black text-xl md:text-4xl text-center">
           Contact Us
         </div>
