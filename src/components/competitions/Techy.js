226e4002a99b2c9e8e5334fbed3b7624736f69f6
devulapallisai <ee20btech11013@iith.ac.in>
2022-03-10 09:57:25 +0530

    added

--- src/components/competitions/Techy.js
+++ src/components/competitions/Techy.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "../../css/events.module.css";
 import line from "../../images/events/line gols 9.png";
-import lineHz from "../../images/events/line gols 8.png";
-import Zakir from "../../images/events/Coming Soon (1).png";
-import {TechyData} from './TechyData'
-import Card from './CCard'
+// import lineHz from "../../images/events/line gols 8.png";
+// import Zakir from "../../images/events/Coming Soon (1).png";
+import { TechyData } from "./TechyData";
+import enig from "../../images/competitions/enigmaa.jpg";
+import app from "../../images/competitions/appdev.jpg";
+import Card from "./CCard";
 function Techy() {
   return (
     <>
@@ -12,7 +14,51 @@ function Techy() {
         <div
           className={`container mx-auto grid md:grid-cols-11 ${styles.gridcont}`}
         >
-             {TechyData.map((item) => (
+          <Card
+            type="col-span-3 mx-0"
+            text="Find the mysterious secret techniques to decrypt the given texts/messages. Clear all the levels in the allotted time to become the ultimate cryptacker. It's a pen and paper event. Each team has to decode a series of crypted messages."
+            title="Engima"
+            image={enig}
+            learnmore={
+              "https://docs.google.com/document/d/1wzHGJylOVHgLyCJRqWtxTvXHdsSQmRS9vXzJYL_5YOw/edit?usp=sharing"
+            }
+            visibility="hidden"
+            // submit={item[0]["sublink"]}
+            // register={item[0]["reglink"]}
+          />
+          <img
+            src={line}
+            alt="imagehere"
+            loading="lazy"
+            className="my-auto mx-auto col-span-1 hidden md:block"
+          ></img>
+          <Card
+            type="col-span-3 mx-0"
+            text="Find the mysterious secret techniques to decrypt the given texts/messages. Clear all the levels in the allotted time to become the ultimate cryptacker. It's a pen and paper event. Each team has to decode a series of crypted messages."
+            title="Engima"
+            image={enig}
+            learnmore={
+              "https://docs.google.com/document/d/1wzHGJylOVHgLyCJRqWtxTvXHdsSQmRS9vXzJYL_5YOw/edit?usp=sharing"
+            }
+            // submit={item[0]["sublink"]}
+            // register={item[0]["reglink"]}
+          />
+          <img
+            src={line}
+            alt="imagehere"
+            loading="lazy"
+            className="my-auto mx-auto col-span-1 hidden md:block"
+          ></img>
+          <Card
+            type="col-span-3 mx-0"
+            text="This competition challenges participants to create innovative applications on mobile platforms - Android, iOS and Windows Phone. Grab the chance to Influence with creativity and innovation by designing a peculiar app."
+            title="App Developement"
+            image={app}
+            learnmore="https://docs.google.com/document/d/1ie-VxbSXJKeCgXwReFTGrON45q6yJkiJgXBOpd4J-ls/edit?usp=sharing"
+            // submit={item[0]["sublink"]}
+            // register={item[0]["reglink"]}
+          />
+          {TechyData.map((item) => (
             <>
               {item[0] && (
                 <>
@@ -71,7 +117,7 @@ function Techy() {
       </div>
       {/* Mobile Part begins */}
       <div className="md:hidden overflow-hidden mx-10 ">
-      {/* {groupedcompmob.map((item) => (
+        {/* {groupedcompmob.map((item) => (
           <>
             <div className="grid grid-cols-1 mx-auto">
               <Card
--- src/components/competitions/TechyData.js
+++ src/components/competitions/TechyData.js
@@ -1,6 +1,4 @@
 import pro from "../../images/competitions/pq1.jpg";
-import enig from "../../images/competitions/enigmaa.jpg";
-import app from "../../images/competitions/appdev.jpg";
 import Webd from "../../images/competitions/webdev.jpg";
 import shark from "../../images/competitions/sharktank.jpeg";
 import hack from "../../images/competitions/hackk.jpg";
@@ -11,147 +9,152 @@ import cad from "../../images/competitions/cad1.png";
 import circut from "../../images/competitions/circuit.jpg";
 import depp from "../../images/competitions/deepn.jpg";
 export const TechyData = [
-    [
-      
-      {
-        name: "Enigma",
-        reglink: "https://dare2compete.com/o/jOnBytU?lb=KnvQoHU",
-        sublink: "https://forms.gle/YPDJXae6UJJhJK346",
-        rules:
-          "https://docs.google.com/document/d/1wzHGJylOVHgLyCJRqWtxTvXHdsSQmRS9vXzJYL_5YOw/edit?usp=sharing",
-        img: enig,
-        details:
-          "Find the mysterious secret techniques to decrypt the given texts/messages. Clear all the levels in the allotted time to become the ultimate cryptacker. It's a pen and paper event. Each team has to decode a series of crypted messages.",
-      },
-      {
-        name: "App Developement",
-        reglink:
-          "https://dare2compete.com/creative-cultural-event/glitz-glamour-elan-ivision-2022-iit-hyderabad-272835",
-        sublink: "https://forms.gle/YyZAgE2VRCGzUTrE8",
-        rules:
-          "https://docs.google.com/document/d/1ie-VxbSXJKeCgXwReFTGrON45q6yJkiJgXBOpd4J-ls/edit?usp=sharing",
-        img: app,
-        details:
-          "This competition challenges participants to create innovative applications on mobile platforms - Android, iOS and Windows Phone. Grab the chance to Influence with creativity and innovation by designing a peculiar app.",
-      },
-      {
-        name: "Proquest",
-        reglink: "https://dare2compete.com/p/proquest-elan-ivision-2022-iit-hyderabad-indian-institute-of-technology-iit-hyderabad-272332",
-        sublink: "https://forms.gle/BL8zB7cgYiv1d6Mk8",
-        rules:
-          "https://docs.google.com/document/d/1AGet3_bye_f7wAeF0sRZg6ALPzB10HjhtUf4pqE1A4U/edit?usp=sharing",
-        img: pro,
-        details:
-          "Experience the Competitive Programming Competition , for all participants of all levels of expertise. This event ensures that even an absolute beginner is able to solve something",
-      },
-    ],
-    [
-      {
-        name: "Web Development",
-        reglink:
-          "https://dare2compete.com/creative-cultural-event/filmfare-fiesta-elan-ivision-2022-iit-hyderabad-indian-institute-of-technology-iit-hyderabad-272833",
-        sublink: "https://forms.gle/brEouAmZhbjghXwQA",
-        rules:
-          "https://docs.google.com/document/d/1xI7zxuOaYU_Zcnk3KNkzp0Aawwi9H1l8eKqIqtC1lg4/edit?usp=sharing",
-        img: Webd,
-        details:
-        "An event that empowers the creative and technically proficient future Web professionals.Participants can Innovate and Inspire by designing a creative Website in this competition."
+  // [
+  //   {
+  //     name: "Enigma",
+  //     reglink: "https://dare2compete.com/o/jOnBytU?lb=KnvQoHU",
+  //     sublink: "https://forms.gle/YPDJXae6UJJhJK346",
+  //     rules:
+  //       "https://docs.google.com/document/d/1wzHGJylOVHgLyCJRqWtxTvXHdsSQmRS9vXzJYL_5YOw/edit?usp=sharing",
+  //     img: enig,
+  //     details:
+  //       "Find the mysterious secret techniques to decrypt the given texts/messages. Clear all the levels in the allotted time to become the ultimate cryptacker. It's a pen and paper event. Each team has to decode a series of crypted messages.",
+  //   },
+  //   {
+  //     name: "App Developement",
+  //     reglink:
+  //       "https://dare2compete.com/creative-cultural-event/glitz-glamour-elan-ivision-2022-iit-hyderabad-272835",
+  //     sublink: "https://forms.gle/YyZAgE2VRCGzUTrE8",
+  //     rules:
+  //       "https://docs.google.com/document/d/1ie-VxbSXJKeCgXwReFTGrON45q6yJkiJgXBOpd4J-ls/edit?usp=sharing",
+  //     img: app,
+  //     details:
+  //       "This competition challenges participants to create innovative applications on mobile platforms - Android, iOS and Windows Phone. Grab the chance to Influence with creativity and innovation by designing a peculiar app.",
+  //   },
+
+  // ],
+  [
+    {
+      name: "Web Development",
+      reglink:
+        "https://dare2compete.com/creative-cultural-event/filmfare-fiesta-elan-ivision-2022-iit-hyderabad-indian-institute-of-technology-iit-hyderabad-272833",
+      sublink: "https://forms.gle/brEouAmZhbjghXwQA",
+      rules:
+        "https://docs.google.com/document/d/1xI7zxuOaYU_Zcnk3KNkzp0Aawwi9H1l8eKqIqtC1lg4/edit?usp=sharing",
+      img: Webd,
+      details:
+        "An event that empowers the creative and technically proficient future Web professionals.Participants can Innovate and Inspire by designing a creative Website in this competition.",
     },
-      {
-        name: "Shark Tank",
-        reglink: "https://dare2compete.com/o/JBdSQcm?lb=KnvQoHU",
-        sublink: "https://forms.gle/7o7Ur19UWNw5GhPJA",
-        rules:
-          "https://docs.google.com/document/d/1Y20CQekNwFNpwau_yT6YjKdtiQxQnBUUUvNhRR-r3ys/edit?usp=sharing",
-        img: shark,
-        details:
-          "Pitch is the first thing that is required by any entrepreneur. In this event participants have to pitch their ideas in front of a large audience in an online mode.",
-      },
-      {
-        name: "Circuit Building",
-        reglink: "https://dare2compete.com/o/2Lvyc1o?lb=KnvQoHU",
-        sublink: "https://forms.gle/9FoS6Bg5kiUt54nE7",
-        rules:
-          "https://docs.google.com/document/d/13lzjGhe8wtDXJMNlA6aiPhRAJ4vCR6fMo1UVQuJ-V0Q/edit?usp=sharing",
-        img: circut,
-        details:
-          "The aim of this competition is to judge the problem solving skills and understanding of the basic circuit designing concepts of the students. Each team has to build a circuit as per the requirement keeping in mind the efficiency factor.",
-      },
-    ],
-    [
-      {
-        name: "Code Arduino",
-        reglink: "https://dare2compete.com/o/Un47bgp?lb=KnvQoHU",
-        sublink: "https://forms.gle/8T9GwzxzdqSSu98P7",
-        rules:
-          "https://docs.google.com/document/d/1ZDsGh_YZHdFvkI4qK2g-gUwfibrUwKd5PLvisbOOj20/edit?usp=sharing",
-        img: arud,
-        details:
-          "The competition aims to test the knowledge of participants on Arduino IDE.",
-      },
-      {
-        name: "Machina Doctrina",
-        reglink:
-          "https://dare2compete.com/creative-cultural-event/art-attack-elan-ivision-2022-iit-hyderabad-272827",
-        sublink: "https://forms.gle/fyyrHb4E9MQh5Rmf6",
-        rules:
-          "https://docs.google.com/document/d/17bkoRtcd99baTlUBkFts6MF1GYxZYOR1q4tN4NHtS3Y/edit?usp=sharing",
-        img: mdd,
-        details:
-          "If making machines learn is your forté, this is the perfect event for you. Test and hone your skills in ML and Data Analytics by participating in Machina doctrinas, one of the coolest events in the fest!.",
-      },
-      {
-        name: "DeepN",
-        reglink: "https://dare2compete.com/o/5wkyMb7?lb=KnvQoHU",
-        sublink: "https://forms.gle/bMcrbbZ2MWc4xrE9A",
-        rules:
-          "https://docs.google.com/document/d/1nMJd8YV4yNsFr0sS9IOW893k_TfFb8fGWfsRnGDGaRY/edit?usp=sharing",
-        img: depp,
-        details: "If Technology had a face now, it would be ‘AI’. An on-spot challenge that would test your understanding of Deep Learning.",
-      },
-    ],
-    [
-        {
-          name: "CAD PRO",
-          reglink: "https://dare2compete.com/o/Un47bgp?lb=KnvQoHU",
-          sublink: "https://forms.gle/8T9GwzxzdqSSu98P7",
-          rules:
-            "https://docs.google.com/document/d/1yPmSpc-j63HQRnJHIozlBusZvOL_7gOPc4C2eoDhYYs/edit?usp=sharing",
-          img: cad,
-          details:
-            "Contrive a way to outsmart your opponents in designing a CAD model.An on-the-spot designing round to show off your expertise in designing.",
-        },
-        {
-          name: "Hackathon",
-          reglink:
-            "https://dare2compete.com/hackathon/hackathon-elan-ivision-2022-iit-hyderabad-indian-institute-of-technology-iit-hyderabad-272342",
-          sublink: "https://forms.gle/fyyrHb4E9MQh5Rmf6",
-          rules:
-            "https://docs.google.com/document/d/14xp2tSHajqrY-i9RYA3ZKYcLm2yJnLEZMCTBXbZz_G8/edit?usp=sharing",
-          img: hack,
-          details:
-            "A unique hackathon on emerging digital technologies to engage with tech enthusiasts. It offers an array of challenges across multiple levels.",
-        },
-        {
-          name: "Paper Presentation",
-          reglink: "https://dare2compete.com/competition/paper-presentation-elan-ivision-2022-iit-hyderabad-indian-institute-of-technology-iit-hyderabad-272356",
-          sublink: "https://forms.gle/bMcrbbZ2MWc4xrE9A",
-          rules:
-            "https://docs.google.com/document/d/1EN1j2047opvegnPWvpc948hF6BheRCR77HRnf9lSbO8/edit?usp=sharing",
-          img: paperpe,
-          details: "In this event, contestants prepare a presentation that showcases and effectively presents new ideas related to any field or even an entire business plan. The main idea of the event is to analyze a topic and think with a new bent of mind.",
-        },
-      ],
-      [
-        {
-          name: "Game Jam",
-          reglink: "https://dare2compete.com/competition/paper-presentation-elan-ivision-2022-iit-hyderabad-indian-institute-of-technology-iit-hyderabad-272356",
-          sublink: "https://forms.gle/bMcrbbZ2MWc4xrE9A",
-          rules:
-            "https://docs.google.com/document/d/1EN1j2047opvegnPWvpc948hF6BheRCR77HRnf9lSbO8/edit?usp=sharing",
-          img: paperpe,
-          details: "Developing a game is the new meta. Game Jam is an event that enables one to create a game from scratch and within the time barrier.",
-        }
-      ]
-  ];
-  
+    {
+      name: "Shark Tank",
+      reglink: "https://dare2compete.com/o/JBdSQcm?lb=KnvQoHU",
+      sublink: "https://forms.gle/7o7Ur19UWNw5GhPJA",
+      rules:
+        "https://docs.google.com/document/d/1Y20CQekNwFNpwau_yT6YjKdtiQxQnBUUUvNhRR-r3ys/edit?usp=sharing",
+      img: shark,
+      details:
+        "Pitch is the first thing that is required by any entrepreneur. In this event participants have to pitch their ideas in front of a large audience in an online mode.",
+    },
+    {
+      name: "Circuit Building",
+      reglink: "https://dare2compete.com/o/2Lvyc1o?lb=KnvQoHU",
+      sublink: "https://forms.gle/9FoS6Bg5kiUt54nE7",
+      rules:
+        "https://docs.google.com/document/d/13lzjGhe8wtDXJMNlA6aiPhRAJ4vCR6fMo1UVQuJ-V0Q/edit?usp=sharing",
+      img: circut,
+      details:
+        "The aim of this competition is to judge the problem solving skills and understanding of the basic circuit designing concepts of the students. Each team has to build a circuit as per the requirement keeping in mind the efficiency factor.",
+    },
+  ],
+  [
+    {
+      name: "Code Arduino",
+      reglink: "https://dare2compete.com/o/Un47bgp?lb=KnvQoHU",
+      sublink: "https://forms.gle/8T9GwzxzdqSSu98P7",
+      rules:
+        "https://docs.google.com/document/d/1ZDsGh_YZHdFvkI4qK2g-gUwfibrUwKd5PLvisbOOj20/edit?usp=sharing",
+      img: arud,
+      details:
+        "The competition aims to test the knowledge of participants on Arduino IDE.",
+    },
+    {
+      name: "Machina Doctrina",
+      reglink:
+        "https://dare2compete.com/creative-cultural-event/art-attack-elan-ivision-2022-iit-hyderabad-272827",
+      sublink: "https://forms.gle/fyyrHb4E9MQh5Rmf6",
+      rules:
+        "https://docs.google.com/document/d/17bkoRtcd99baTlUBkFts6MF1GYxZYOR1q4tN4NHtS3Y/edit?usp=sharing",
+      img: mdd,
+      details:
+        "If making machines learn is your forté, this is the perfect event for you. Test and hone your skills in ML and Data Analytics by participating in Machina doctrinas, one of the coolest events in the fest!.",
+    },
+    {
+      name: "DeepN",
+      reglink: "https://dare2compete.com/o/5wkyMb7?lb=KnvQoHU",
+      sublink: "https://forms.gle/bMcrbbZ2MWc4xrE9A",
+      rules:
+        "https://docs.google.com/document/d/1nMJd8YV4yNsFr0sS9IOW893k_TfFb8fGWfsRnGDGaRY/edit?usp=sharing",
+      img: depp,
+      details:
+        "If Technology had a face now, it would be ‘AI’. An on-spot challenge that would test your understanding of Deep Learning.",
+    },
+  ],
+  [
+    {
+      name: "CAD PRO",
+      reglink: "https://dare2compete.com/o/Un47bgp?lb=KnvQoHU",
+      sublink: "https://forms.gle/8T9GwzxzdqSSu98P7",
+      rules:
+        "https://docs.google.com/document/d/1yPmSpc-j63HQRnJHIozlBusZvOL_7gOPc4C2eoDhYYs/edit?usp=sharing",
+      img: cad,
+      details:
+        "Contrive a way to outsmart your opponents in designing a CAD model.An on-the-spot designing round to show off your expertise in designing.",
+    },
+    {
+      name: "Hackathon",
+      reglink:
+        "https://dare2compete.com/hackathon/hackathon-elan-ivision-2022-iit-hyderabad-indian-institute-of-technology-iit-hyderabad-272342",
+      sublink: "https://forms.gle/fyyrHb4E9MQh5Rmf6",
+      rules:
+        "https://docs.google.com/document/d/14xp2tSHajqrY-i9RYA3ZKYcLm2yJnLEZMCTBXbZz_G8/edit?usp=sharing",
+      img: hack,
+      details:
+        "A unique hackathon on emerging digital technologies to engage with tech enthusiasts. It offers an array of challenges across multiple levels.",
+    },
+    {
+      name: "Paper Presentation",
+      reglink:
+        "https://dare2compete.com/competition/paper-presentation-elan-ivision-2022-iit-hyderabad-indian-institute-of-technology-iit-hyderabad-272356",
+      sublink: "https://forms.gle/bMcrbbZ2MWc4xrE9A",
+      rules:
+        "https://docs.google.com/document/d/1EN1j2047opvegnPWvpc948hF6BheRCR77HRnf9lSbO8/edit?usp=sharing",
+      img: paperpe,
+      details:
+        "In this event, contestants prepare a presentation that showcases and effectively presents new ideas related to any field or even an entire business plan. The main idea of the event is to analyze a topic and think with a new bent of mind.",
+    },
+  ],
+  [
+    {
+      name: "Game Jam",
+      reglink:
+        "https://dare2compete.com/competition/paper-presentation-elan-ivision-2022-iit-hyderabad-indian-institute-of-technology-iit-hyderabad-272356",
+      sublink: "https://forms.gle/bMcrbbZ2MWc4xrE9A",
+      rules:
+        "https://docs.google.com/document/d/1EN1j2047opvegnPWvpc948hF6BheRCR77HRnf9lSbO8/edit?usp=sharing",
+      img: paperpe,
+      details:
+        "Developing a game is the new meta. Game Jam is an event that enables one to create a game from scratch and within the time barrier.",
+    },
+    {
+      name: "Proquest",
+      reglink:
+        "https://dare2compete.com/p/proquest-elan-ivision-2022-iit-hyderabad-indian-institute-of-technology-iit-hyderabad-272332",
+      sublink: "https://forms.gle/BL8zB7cgYiv1d6Mk8",
+      rules:
+        "https://docs.google.com/document/d/1AGet3_bye_f7wAeF0sRZg6ALPzB10HjhtUf4pqE1A4U/edit?usp=sharing",
+      img: pro,
+      details:
+        "Experience the Competitive Programming Competition , for all participants of all levels of expertise. This event ensures that even an absolute beginner is able to solve something",
+    },
+  ],
+];
