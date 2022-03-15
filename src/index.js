2e0d53f3307f3d26adc2e12b234085f933b26314
arun <arun@arunsiddardha.iith.ac.in>
2022-03-16 02:01:09 +0530

    done

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "./css/index.css";
 import "./fonts/LemonMilk/LemonMilk.otf";
 import "./fonts/LemonMilk/LemonMilkbold.otf";
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,7 +13,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
