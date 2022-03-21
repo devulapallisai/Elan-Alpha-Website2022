720f0c626abafa5485c1242e0136439b79e2ec08
ArunSiddardha <arunsiddardha19@gmail.com>
2022-03-21 20:52:05 +0530

    Done

--- src/components/proshows/Mobile-card.js
+++ src/components/proshows/Mobile-card.js
@@ -3,8 +3,8 @@ import styles from '../../css/proshow.module.css'
 function MCard({image , description}) {
   return (
   <div className={`grid grid-cols-1 border-2 border-[#FFD700] w-5/6 p-5 gap-5 `} >
-      <img className={`col-span-1 ${styles.imageBox}`} src={image} alt= "image"></img>
-      <div className={`col-span-1 flex items-center justify-center text-xs w-fit ${styles.proText}`} >    
+      <img className={`col-span-1 ${styles.imageBox} z-[10000]`} src={image} alt= "image"></img>
+      <div className={`col-span-1 flex items-center  text-white justify-center text-xs w-fit ${styles.proText}`} >    
       {description}
       </div>
    </div>
--- src/components/proshows/Proshow-card.js
+++ src/components/proshows/Proshow-card.js
@@ -2,9 +2,9 @@ import React from 'react'
 import styles from '../../css/proshow.module.css'
 function Card({image , description}) {
   return (
-  <div className={`flex border-2 border-[#FFD700] w-5/6 p-5 gap-5 `}  >
-      <img className={` ${styles.imageBox}`} src={image} alt= "image"></img>
-      <div className={`flex items-center justify-center text-md w-fit ${styles.proText}`} >    
+  <div className={`flex border-2 border-[#FFD700] w-5/6 p-5 gap-5 z-3 `}  >
+      <img className={` ${styles.imageBox} z-[10000]`} src={image} alt= "image"></img>
+      <div className={`flex items-center text-white justify-center text-md w-fit ${styles.proText}`} >    
       {description}
       </div>
    </div>
