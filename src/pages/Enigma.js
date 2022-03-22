5b1daa033c6d99ae4180a8f8c8270bac0e046784
ArunSiddardha <arunsiddardha19@gmail.com>
2022-03-22 20:12:46 +0530

    fone

--- src/pages/Enigma.js
+++ src/pages/Enigma.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Enigma() {
   return (
     <div className='h-screen w-screen bg-black flex gap-5 flex-col justify-center text-xl items-center'>
-   <h1 className=' md:text-[2rem] text-center'>Congrats on Finishing Enigma</h1>
-   <h2 className=' md:text-[1.5rem]  text-center'>Answer for last question is : We-will-win-Enigma23-too!</h2>
+   <h1 className=' md:text-[2rem] text-white text-center'>Congrats on Finishing Enigma</h1>
+   <h2 className=' md:text-[1.5rem] text-white  text-center'>Answer for last question is : GG_3n1gm4!!</h2>
     </div>
   )
 }
